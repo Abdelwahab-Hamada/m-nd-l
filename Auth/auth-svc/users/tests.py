9750055244f3a0cead7571a023c359f0c1d029bf
abdelwahab youssef <abdelwahab.hamada@gmail.com>
2023-01-09 18:29:26 +0200

    1st tests

--- Auth/auth-svc/users/tests.py
+++ Auth/auth-svc/users/tests.py
@@ -1,2 +1,23 @@
 from django.test import TestCase
+from .models import User
+from django.contrib.auth import authenticate
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='',name='tester',password='12345678')
+
+    def test_user_can_signin(self):
+        tester=authenticate(username='tester',password='12345678')
+        
+        self.assertEqual(tester.name,'tester')
+    
+    def test_user_can_update_account(self):
+        tester=authenticate(username='tester',password='12345678')
+        tester.name='tester2'
+        tester.save()
+
+        self.assertEqual(tester.name,'tester2')
+
+        
 
